d33866548394a5e3beb6fffb6a49b4649958809e
RickyIG <rickyindra53@gmail.com>
2024-06-01 19:13:06 +0700

    update requirements 5

--- main.py
+++ main.py
@@ -32,7 +32,9 @@ import math
 import time
 import shutil
 import pickle
+from spacy.cli import download
 
+download("en_core_web_sm")
 # Versi Library.
 print("NumPy version:", np.__version__)
 print("Pandas version:", pd.__version__)
